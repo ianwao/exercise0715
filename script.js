3ab6321747c0d6aa2da2f5532d10b3a91344a907
FBH <FBH@DESKTOP-GFIDQDA>
2019-07-15 17:31:43 +0800

    js

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-111111111
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+
